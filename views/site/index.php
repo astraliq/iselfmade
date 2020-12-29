d2d94f2dd79f3e2ffe13269ae4c34ae0f55d132f
astraliq <astraliq457@gmail.com>
2020-12-29 21:52:14 +0300

    верстка начальной страницы интегрирована

--- views/site/index.php
+++ views/site/index.php
@@ -5,17 +5,239 @@
 ?>
 
 <main class="main__style">
-    <h1 class="main__title">iselfmade.ru — сервис для достижения личных целей и выполнения задач</h1>
-    <p class="main__text">
-        Никакой магии или «секретной» методики. Большинство инструментов и способов вам известно уже давно. К&nbsp;сожалению, просто знания никак не продвигают нас к своим целям, только действия и ничего больше. Мы создали этот сервис, чтобы вы оказались в ситуации,
-        когда проще сделать, чем не сделать.
-    </p>
-    <p class="main__text">
-        Делая каждый день, то что нужно, вы постепенно приближаетесь к своим целям.
-    </p>
-    <section class="about">
-        <h2 class="h2__title">Что такое iselfmade.ru</h2>
-        <img class="about__img">
-        <p class="about__img-text">Раздел «Отчёт». Самый важный раздел</p>
+    <section class="f-screen__style">
+        <div class="f-screen__left">
+            <p class="f-screen__left-text">Текущая дата, день недели, неделя и день в году</p>
+            <p class="f-screen__left-text">Дополнительные сервисы помогут дойти до конца</p>
+        </div>
+        <div class="f-screen__center">
+            <p class="f-screen__center-text">
+                Цели на год и задачи на текущий месяц всегда перед вами, как напоминание.<br>Так эффективнее планировать дела на день.
+            </p>
+            <img class="f-screen__img" src="img/screens/f-screen.png" width="1098" height="640" alt="Главный экран. Отчёт">
+            <p class="f-screen__center-caption">
+                Раздел &laquo;Отчёт&raquo;.<br>Это основной раздел, с которым вам придётся работать. <br>Тут вы отчитываетесь за сегодняшний день и планируете следующий.
+            </p>
+        </div>
+        <div class="f-screen__right">
+            <p class="f-screen__right-text">
+                Аватарка пользователя и дополнительное меню с настройками
+            </p>
+            <p class="f-screen__right-text">
+                Коэффециент выполнения дел за день. Влияет на условия работы с сервисом.
+            </p>
+        </div>
+    </section>
+    <section class="title container">Как это работает</section>
+    <section class="curator">
+        <div class="container">
+            <h2 class="chapter__title">Персональный куратор</h2>
+            <p class="curator__text">
+                У вас будет <strong>персональный куратор</strong>. Он будет проверять каждый отчёт, который вы заполняете в системе. А в тарифах &laquo;Результат&raquo; и &laquo;Прорыв&raquo;, куратор ещё и <strong>звонит вам по телефону</strong>,
+                чтобы узнать как много дел вы уже выполнили сегодня.
+            </p>
+            <div class="curator__sound">
+                <div class="curator__sound-oval">
+                    <div class="curator__sound-round" id="sound">►</div>
+                    <p class="curator__sound-text">Фрагмент разговора с куратором</p>
+                </div>
+            </div>
+            <p class="curator__text">
+                Для некоторых дел, которые вы указали в отчёте, вашему куратору могут потребоваться «доказательства». Например, скрин подготовленного письма или составленное резюме для поиска новой работы.
+            </p>
+            <div class="curator__img">
+                <img src="img/screens/curator-messages.png" width="555" height="370" alt="Фрагмент переписки с куратором">
+                <p class="curator__caption">Фрагмент чата с куратором.</p>
+            </div>
+            <p class="curator__text">
+                Ни нам, ни куратору не нужны конфиденциальные данные или «доказательства» на каждый выполненный пункт. </p>
+            <p class="curator__text">Основная задача — не допустить отчёты–отписки и сделать так, чтобы каждый отчёт был осознанным и достоверным.
+            </p>
+        </div>
+        <div class="container_full">
+            <div class="container">
+                <p class="curator__info">
+                    Куратор верит в вас.<br>Не подведите его — сделайте уже то, что запланировали.
+                </p>
+            </div>
+        </div>
+    </section>
+    <section class="tutor">
+        <div class="container">
+            <h2 class="chapter__title">Человек, перед которым стыдно облажаться</h2>
+            <p class="tutor__text">
+                <strong>Наставник</strong> &#151; человек, перед которым вам лично будет очень неудобно, если он узнает, что вы не выполнили то, что планировали и тем самым не достигли намеченных целей.
+            </p>
+            <p class="tutor__text">
+                В детстве, чаще всего, для нас это была мама (&laquo;только бы мама не узнала&raquo;). Во взрослой жизни это человек, который добился больше. В чём&#150;то он наш ориентир, тот, на кого мы хотим быть похожи.
+            </p>
+            <p class="tutor__text">
+                Укажите его почту и он будет получать сводный отчёт о ваших успехах. И представьте, как потом будете смотреть ему в глаза.
+            </p>
+            <div class="tutor__img">
+                <img src="img/screens/tutor-mail.png" width="768" height="473" alt="Сводный отчёт наставнику">
+                <p class="tutor__caption">Сводный отчёт, который получает наставник.</p>
+            </div>
+        </div>
+    </section>
+    <section class="section_container">
+        <div class="chapter">
+            <h2 class="chapter__title">Работы с ментором</h2>
+            <p class="chapter__text">Если у вас есть ментор, то укажите его почту, чтобы и он получал сводный отчёт ваших успехов.</p>
+            <p class="chapter__text">Если же ментора нет, то вы можете найти его в нашем списке менторов. Мы тчательно состовляем этот список, поэтому совершенно уверены в профессионализме каждого, кто там указан.</p>
+        </div>
+        <div class="chapter">
+            <h2 class="chapter__title">Группа поддержки</h2>
+            <p class="chapter__text">В достижении целей важно ощущать поддержку людей, которые верят в тебя.<br>Приглашайте в &laquo;НОСОРОГ&raquo; своих друзей и знакомых, и создавайте свою группу поддержки.</p>
+            <p class="chapter__text">Если своей группы поддержки нет, то можно присоедениться к уже существующим. Группы, в которых есть свободные места, размещаны в соответствующем разделе.</p>
+        </div>
+        <div class="chapter">
+            <h2 class="chapter__title">Коэффициент выполнения</h2>
+            <p class="chapter__text">По результатам «рабочего дня» система рассчитывает ваш коэффециент выполнения.<br>Не допускайте, чтобы коэффециент был ниже 60%.</p>
+            <p class="chapter__text">Если такое случится <strong>5 дней</strong> в текущем месяце, то система блокирует вашу учётную запись. Восстановление возможно только через техническую поддержку.</p>
+        </div>
+    </section>
+    <section class="container_full container_full-240">
+        <div class="container">
+            Система &laquo;НОСОРОГ&raquo;&nbsp;&#151; не просто программа для списка дел.<br> Это целый набор сервисов, которые помогают вам делать то, что вы запланировали.
+        </div>
+    </section>
+    <section class="other">
+        <div class="container">
+            <p class="title">Дополнительные сервисы</p>
+            <h2 class="chapter__title">Доска эффективности</h2>
+            <p class="chapter__text">
+                На этой доске собраны статистические данные ваших успехов и достижений. Сколько дел было выполненно, сколько задач месяца вы завершили и каких целей уже добились.
+            </p>
+            <div class="tutor__img">
+                <img src="img/screens/tutor-mail.png" width="768" height="473" alt="Сводный отчёт наставнику">
+                <p class="tutor__caption">Доска эффективности пользователя</p>
+            </div>
+            <div class="chapter">
+                <h2 class="chapter__title">Повторяющиеся дела</h2>
+                <p class="chapter__text">Укажите, какие дела вы планируете делать постоянно, внесите их в специальный раздел и они будут появляться в плане нужного дня.</p>
+            </div>
+            <div class="chapter">
+                <h2 class="chapter__title">Будущие дела</h2>
+                <p class="chapter__text">Если вы уже сейчас знаете, что нужно будет сделать в конкретный день в будщем, то запишите это в этот раздел и система сама включит это дело в нужный день.</p>
+            </div>
+            <div class="chapter">
+                <h2 class="chapter__title">мои обещания</h2>
+                <p class="chapter__text">Записывайте что и кому обещали сделать.</p>
+            </div>
+            <div class="chapter">
+                <h2 class="chapter__title">Возможные дела</h2>
+                <p class="chapter__text">Всё, что вам кажется может быть придётся делать в будущем. Записывайте, чтобы не забыть.</p>
+            </div>
+            <div class="other__button">
+                <button class="other__btn btn__shadow">Посмотреть все сервисы</button>
+            </div>
+        </div>
+    </section>
+    <section class="container_full container_full-280">
+        <div class="container">
+            Мы постепенно добавляем новые сервисы.<br> Есть идея нового сервиса?<br>
+            <a class="plate-link" href="#">Напишите нам</a> любым удобным вам способом.<br> Готовы обсуждать.
+        </div>
+    </section>
+    <section class="rates">
+        <div class="container">
+            <p class="title">Тарифы</p>
+        </div>
+        <div class="rates__items">
+            <div class="rates__item rates-text">
+                <p class="rates__text">
+                    Название тарифа и его стоимость
+                </p>
+                <p class="rates__text">
+                    <strong>Бесплатный период</strong>
+                </p>
+                <p class="rates__text">
+                    Количество «<strong>рабочих дней</strong>», когда нужно писать отчёт (в неделю)
+                </p>
+                <p class="rates__text">
+                    Количество дней, когда <strong>можно пропустить</strong> сдачу отчёта (в месяц)
+                </p>
+                <p class="rates__text">
+                    <strong>Минимальный</strong> коэффециент выполнения дневных дел
+                </p>
+                <p class="rates__text">
+                    Работа с целями, задачами и делами на день
+                </p>
+                <p class="rates__text">
+                    Отчёт принимает куратор
+                </p>
+                <p class="rates__text">
+                    Отправка сводного отчёта наставнику/ментору
+                </p>
+                <p class="rates__text">
+                    Группа поддержки
+                </p>
+                <p class="rates__text">
+                    Звонок от куратора (<strong>3 раза в неделю</strong>)
+                </p>
+                <p class="rates__text">
+                    Звонок от куратора (<strong>каждый рабочий день</strong>)
+                </p>
+            </div>
+            <div class="rates__item rates-standart">
+                <p class="rates__name">Тариф «СТАНДАРТ»</p>
+                <p class="rates__price">99 ₽/месяц</p>
+                <p class="rates__free-pay">3 месяца</p>
+                <p class="rates__work-day">5</p>
+                <p class="rates__free-day">1</p>
+                <p class="rates__ratio">75%</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__minus">–</p>
+                <p class="rates__minus">–</p>
+                <p class="rates__minus">–</p>
+                <div class="rates__button">
+                    <button class="rates__btn btn__shadow">Попробовать бесплатно</button>
+                </div>
+            </div>
+            <div class="rates__item rates-result">
+                <p class="rates__name">Тариф «РЕЗУЛЬТАТ»</p>
+                <p class="rates__price">249 ₽/месяц</p>
+                <p class="rates__free-pay">&#151;</p>
+                <p class="rates__work-day">6</p>
+                <p class="rates__free-day">2</p>
+                <p class="rates__ratio">70%</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__minus">–</p>
+                <div class="rates__button">
+                    <!-- <button class="rates__btn" disabled>Начать</button> -->
+                    <p class="rates__name">Доступен с 01.03.2021</p>
+                </div>
+            </div>
+            <div class="rates__item rates-break">
+                <p class="rates__name">Тариф «ПРОРЫВ»</p>
+                <p class="rates__price">749 ₽/месяц</p>
+                <p class="rates__free-pay">&#151;</p>
+                <p class="rates__work-day">6</p>
+                <p class="rates__free-day">3</p>
+                <p class="rates__ratio">75%</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__plus">+</p>
+                <p class="rates__minus">–</p>
+                <p class="rates__plus">+</p>
+                <div class="rates__button">
+                    <!-- <button class="rates__btn" disabled>Начать</button> -->
+                    <p class="rates__name">Доступен с 01.03.2021</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="rhino">
+        <p class="rhino__text">
+            Носорог&nbsp;&#151; спокойное и упрямое животное. <br>Своим рогом расчищает себе путь: ломает ветки, стоящие на его пути. При опасности может рогом нанести сокрушительный удар врагу.
+        </p>
     </section>
 </main>
