ce68949e8b5480eb8755035288b6f996cb174da7
astraliq <astral457@mail.ru>
2024-07-13 09:46:37 +0300

    update add db config

--- common/config/main.php
+++ common/config/main.php
@@ -6,6 +6,34 @@ return [
     ],
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=' . $_ENV['DB_HOST'] . ';port=' . $_ENV['DB_HOST_PORT'] . ';dbname=iselfmade',
+            'username' => $_ENV['DB_USERNAME'],
+            'password' => $_ENV['DB_PASSWORD'],
+            'charset' => 'utf8',
+
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 7200,
+            // Name of the cache component used to store schema information
+            'schemaCache' => 'frontendCache',
+
+            'enableQueryCache' => true,
+            'queryCacheDuration' => 7200,
+            'queryCache' => 'frontendCache',
+        ],
+        'mailer' => [
+            'class' => 'yii\swiftmailer\Mailer',
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host'     => $_ENV['MAILER_HOST'],
+                'port'     => $_ENV['MAILER_PORT'], // 25 | 465 | 587
+                'username' => $_ENV['MAILER_USERNAME'],
+                'password' => $_ENV['MAILER_PASSWORD'],
+                'encryption' => 'ssl', // tls | ssl
+            ],
+            'viewPath' => '@common/mail',
+        ],
         'cache' => [
             'class' => \yii\caching\FileCache::class,
         ],
